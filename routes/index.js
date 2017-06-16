15f79740e66f46c69a6050191b0cdb8f91895d4c
Kunal Malhotra <kunal.malhotra2007@gmail.com>
2017-06-16 14:22:48 -0700

    kunal

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,11 @@ var data = {};
 
 data.led1 = 200;
 data.led2 = 300;
+data.message = "Hi, Good Morning";
 
 exports.index = function(req, res){
+
+  console.log(data.led1 + ' ' + data.led2);
   res.json(data);
+
 };
